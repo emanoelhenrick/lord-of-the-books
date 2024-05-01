6e48c3fb255e4d570e386494312d07551a402292
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2024-05-01 18:38:55 -0300

    refactor: update brackets styles

--- app/src/main/java/org/minimundo/utils/GoogleBooks/dtos/ItemDto.java
+++ app/src/main/java/org/minimundo/utils/GoogleBooks/dtos/ItemDto.java
@@ -3,5 +3,7 @@ package org.minimundo.utils.GoogleBooks.dtos;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ItemDto(String id, VolumeInfoDto volumeInfo) {
-}
+public record ItemDto(
+  String id,
+  VolumeInfoDto volumeInfo
+) {}
