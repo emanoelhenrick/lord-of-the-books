4cd39f545925410e6af566cffdff21ec9d45919b
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2024-07-25 14:08:12 -0300

    feat: create UserBook model

---
+++ app/src/main/java/org/minimundo/core/domain/models/UserBook.java
@@ -0,0 +1,9 @@
+package org.minimundo.core.domain.models;
+
+public class UserBook {
+  private String id;
+  private String userId;
+  private Book book;
+  public Double progression;
+  public Review review;
+}
