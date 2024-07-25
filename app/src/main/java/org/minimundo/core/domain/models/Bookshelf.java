0ad482f45f2eb6e71bc0f0ca840f4c1416e0a20a
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2024-07-25 00:44:00 -0300

    refactor: replace public to private

--- app/src/main/java/org/minimundo/core/domain/models/Bookshelf.java
+++ app/src/main/java/org/minimundo/core/domain/models/Bookshelf.java
@@ -3,7 +3,7 @@ package org.minimundo.core.domain.models;
 import java.util.ArrayList;
 
 public class Bookshelf {
-  public BookshelfType type;
+  private BookshelfType type;
   private String userId;
   private String username;
   public ArrayList<Book> books;
